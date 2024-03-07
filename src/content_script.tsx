d6e7b0f117d130874f3a908e7e6d8d60696a662c
NahomiTakahashi <nahomi.takahashi@connehito.com>
2024-03-07 20:11:46 +0900

    初期開発完了

--- src/content_script.tsx
+++ src/content_script.tsx
@@ -1,9 +1,73 @@
 chrome.runtime.onMessage.addListener(function (msg, sender, sendResponse) {
-  if (msg.color) {
-    console.log("Receive color = " + msg.color);
-    document.body.style.backgroundColor = msg.color;
-    sendResponse("Change color to " + msg.color);
-  } else {
-    sendResponse("Color message is none.");
-  }
+    // 対象の<th>を取得
+    const ths = document.querySelectorAll("th.jbc-text-sub");
+    // 実労働時間を取得
+    const actualWorkingHoursThElement = Array.from(ths).find((th) => th.textContent === '実労働時間');
+    const actualWorkingHoursTd = actualWorkingHoursThElement?.nextElementSibling;
+    const actualWorkingHoursSpan = actualWorkingHoursTd?.querySelector('.info-content.text-right.text-nowrap.d-inline-block');
+    const actualWorkingHour = actualWorkingHoursSpan?.textContent;
+    console.log('取得した実労働時間のtd:', actualWorkingHour);
+
+    // 月規定労働時間を取得
+    const monthProvisionWorkingHoursThElement = Array.from(ths).find((th) => th.textContent === '月規定労働時間');
+    const monthProvisionWorkingHoursTd = monthProvisionWorkingHoursThElement?.nextElementSibling;
+    const monthProvisionWorkingHoursSpan = monthProvisionWorkingHoursTd?.querySelector('.info-content.text-right.text-nowrap.d-inline-block');
+    const monthProvisionWorkingHours = monthProvisionWorkingHoursSpan?.textContent;
+    console.log('取得した月規定労働時間のtd:', monthProvisionWorkingHours);
+
+    // 平日出勤日数を取得
+    const workingDaysThElement = Array.from(ths).find((th) => th.textContent === '平日出勤日数');
+    const workingDaysTd = workingDaysThElement?.nextElementSibling;
+    const workingDaysSpan = workingDaysTd?.querySelector('.info-content.text-right.text-nowrap.d-inline-block');
+    const workingDays = workingDaysSpan?.textContent;
+    console.log('取得した平日出勤日数のtd:', workingDays);
+
+    // 勤務中の時間を取得
+    const table = document.querySelectorAll(".table-responsive");
+    const trs = table[0].querySelectorAll("tr");
+    const workingTrs = Array.from(trs).filter((tr) => tr.textContent?.includes('勤務中'));
+    const todayWorkingHours = Array.from(workingTrs).map((tr) => tr.querySelector('td:nth-child(6)')?.textContent);
+    console.log('取得した勤務中の時間:', todayWorkingHours[0] ?? '00:00');
+
+    // 所定労働日数を取得
+    const scheduledWorkingDays = Array.from(ths).find((th) => th.textContent === '所定労働日数');
+    const scheduledWorkingDaysTd = scheduledWorkingDays?.nextElementSibling;
+    const actualScheduledWorkingDays = scheduledWorkingDaysTd?.textContent;
+    console.log('取得した所定労働日数:', actualScheduledWorkingDays);
+
+    // 有給時間の取得
+    const footerTable = document.querySelectorAll(".jbc-table-footer");
+    // 9番目のtdを取得
+    const footerTableTd = footerTable[0].querySelectorAll("td");
+    const paidLeaveHoursTd = footerTableTd[8];
+    const paidLeaveHours = paidLeaveHoursTd?.textContent;
+    console.log('取得した有給時間:', paidLeaveHours);
+
+    // 有給を取得
+    const paidLeaveDaysThElement = Array.from(ths).find((th) => th.textContent === '有休(全休)');
+    const paidLeaveDaysTd = paidLeaveDaysThElement?.nextElementSibling;
+    const paidLeaveDaysSpan = paidLeaveDaysTd?.querySelector('.info-content.text-right.text-nowrap.d-inline-block');
+    const paidLeaveDays = paidLeaveDaysSpan?.textContent;
+    console.log('取得した有休(全休)', paidLeaveDays);
+
+    // 振休を取得
+    const substituteHolidayThElement = Array.from(ths).find((th) => th.textContent === '振休(全休)');
+    const substituteHolidayTd = substituteHolidayThElement?.nextElementSibling;
+    const substituteHolidaySpan = substituteHolidayTd?.querySelector('.info-content.text-right.text-nowrap.d-inline-block');
+    const substituteHoliday = substituteHolidaySpan?.textContent;
+    console.log('振休(全休)', substituteHoliday);
+
+
+    // オブジェクトにして返却する
+    const result = {
+      'actualWorkingHour': actualWorkingHour,
+      'workingDays': workingDays,
+      'todayWorkingHours': todayWorkingHours[0] ?? '00:00',
+      'scheduledWorkingDays': actualScheduledWorkingDays,
+      'monthProvisionWorkingHours': monthProvisionWorkingHours,
+      'paidLeaveHours': paidLeaveHours,
+      'paidLeaveDays': paidLeaveDays ?? '0',
+      'substituteHoliday': substituteHoliday ?? '0'
+    }
+    sendResponse(result);
 });
--- src/popup.tsx
+++ src/popup.tsx
@@ -1,51 +1,105 @@
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import { createRoot } from "react-dom/client";
+import styled from "styled-components";
+
+const Container = styled.div`
+  min-width: 350px;
+  padding: 10px;
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+`;
+
+const List = styled.ul`
+   display: flex;
+   flex-direction: column;
+   align-items: flex-start;
+`;
+
+const Button = styled.button`
+  align-self: flex-end;
+  background-color: #FF8080;
+  color: #fff;
+  border: none;
+  padding: 10px 20px;
+  font-size: 16px;
+  cursor: pointer;
+  border-radius: 4px;
+`;
+
+const ListItem = styled.li`
+  margin-bottom: 10px;
+  list-style: none;
+`;
+
+const Comment = styled.p`
+  align-self: flex-end;
+`;
 
 const Popup = () => {
-  const [count, setCount] = useState(0);
-  const [currentURL, setCurrentURL] = useState<string>();
+  const [totalWorkTime, setTotalWorkTime] = useState(0);
+  const [workTime, setWorkTime] = useState(0);
+  const [restWorkTime, setRestWorkTime] = useState(0);
+  const [restWorkDays, setRestWorkDays] = useState(0);
 
-  useEffect(() => {
-    chrome.action.setBadgeText({ text: count.toString() });
-  }, [count]);
-
-  useEffect(() => {
-    chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
-      setCurrentURL(tabs[0].url);
-    });
-  }, []);
-
-  const changeBackground = () => {
-    chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+  const calculateWorkTime = () => {
+      chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
       const tab = tabs[0];
       if (tab.id) {
         chrome.tabs.sendMessage(
           tab.id,
           {
-            color: "#555555",
+            code: `calculateWorkTime`,
           },
-          (msg) => {
-            console.log("result message:", msg);
-          }
+            (response) => {
+                const [hour, minutes] = response['actualWorkingHour'].split(':');
+                const actualWorking = parseFloat(`${hour}.${minutes}`);
+                const [todayHour, todayMinutes] = response['todayWorkingHours'].split(':');
+                const todayActualWorking = parseFloat(`${todayHour}.${todayMinutes}`);
+                const [hour1, minutes2] = response['monthProvisionWorkingHours'].split(':');
+                const provisionWorking = parseFloat(`${hour1}.${minutes2}`);
+                const [hour2, minutes3] = response['paidLeaveHours'].split(':');
+                const totalWorking = actualWorking + parseFloat(`${hour2}.${minutes3}`);
+                const workingDays = parseFloat(response['workingDays']) + parseFloat(response['paidLeaveDays']);
+
+                // 稼働中でない場合
+                if (todayActualWorking === 0) {
+                    setTotalWorkTime(totalWorking);
+                    setWorkTime(totalWorking / workingDays);
+                    const remainDays = response['scheduledWorkingDays'].replace('日', '') - workingDays;
+                    if (remainDays === 0) {
+                        setRestWorkDays(0);
+                        setRestWorkTime(0);
+                        return;
+                    }
+                    setRestWorkDays(remainDays);
+                    setRestWorkTime((provisionWorking - todayActualWorking) / remainDays);
+                    return;
+                }
+
+                const beforeProvisionWorking = totalWorking - todayActualWorking;
+                setWorkTime(beforeProvisionWorking / (workingDays - 1));
+                const remainDays = response['scheduledWorkingDays'].replace('日', '') - (response['workingDays'] - 1)
+                setTotalWorkTime(beforeProvisionWorking);
+                setRestWorkDays(remainDays);
+                setRestWorkTime((provisionWorking - beforeProvisionWorking) / remainDays);
+            }
         );
       }
     });
   };
 
   return (
-    <>
-      <ul style={{ minWidth: "700px" }}>
-        <li>Current URL: {currentURL}</li>
-        <li>Current Time: {new Date().toLocaleTimeString()}</li>
-      </ul>
-      <button
-        onClick={() => setCount(count + 1)}
-        style={{ marginRight: "5px" }}
-      >
-        count up
-      </button>
-      <button onClick={changeBackground}>change background</button>
-    </>
+      <Container>
+          <List>
+              <ListItem>稼働合計時間：{totalWorkTime}</ListItem>
+              <ListItem>平均稼働時間：{workTime}</ListItem>
+              <ListItem>残りの稼働時間/日：{restWorkTime}</ListItem>
+              <ListItem>残りの稼働日：{restWorkDays}</ListItem>
+          </List>
+          <Button onClick={calculateWorkTime}>稼働時間を計算</Button>
+          <Comment>※稼働中の場合は前日までの稼働時間で計算をしています。</Comment>
+      </Container>
   );
 };
 
